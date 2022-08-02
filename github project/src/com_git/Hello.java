f8396c7501dc3a6ffc4218e6bab0e13cd6121e03
amol90r <110409755+amol90r@users.noreply.github.com>
2022-08-02 16:54:44 +0530

    by  git

--- github project/src/com_git/Hello.java
+++ github project/src/com_git/Hello.java
@@ -2,6 +2,11 @@ package com_git;
 
 public class Hello 
 {
+	public void m1()
+	{
+		System.out.println("this is first change in git");
+	}
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("hello");
